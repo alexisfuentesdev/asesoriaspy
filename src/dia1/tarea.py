0ebc4ff30cbdc775f124d55824a33826a18dd5d0
Alexis Fuentes <alexis.a.fuentes@gmail.com>
2020-10-16 20:19:21 -0500

    Todo comentado

--- src/dia1/tarea.py
+++ src/dia1/tarea.py
@@ -11,46 +11,67 @@ def diff(x):
 
 
 def NewRap(f, df, a, tol): #vamos a llamar la función para que realice el método
-
+    # a = -2
 	if pol(a)/diff(a) < 0:
 
-
+        # La impresión de los titulos
 		print ("{:^10}{:^10}{:^10}{:^10}{:^10}{:^10}{:^10}".format
 		  ("i","a","fa","f´d","xr","fxr","ErrorAp"))
 
-	# Inicializamos el contador y encontramos la raíz
+	    # Inicializamos el contador y encontramos la raíz
 
-		i = 1
+        # Asignaciones de datos
+		i = 1 # El numero de la fila
 		xr = a-(pol(a)/diff(a))	#se calcula el 4.3
 		xra = xr 		#ese 4.3 lo guardo en xra
-	
+
+        # impresión de los resultados
+        # El .4f nos mostrara el total de decimales para la impresión
 		print ("{:^10}{:^10.4f}{:^10.4f}{:^10.4f}{:^10.4f}{:^10.4f}".format
-		  (i,float(a),float(pol(a)),float(diff(a)),float(xr),float(pol(xr))))
+		    (
+                i,
+                float(a),
+                float(pol(a)),
+                float(diff(a)),
+                float(xr),
+                float(pol(xr))
+            )
+        )
 
-
+        # Calculamos el error, función de math ABS
 		error = abs(pol(xr)) 	#por mientras para entarr al ciclo
 
-
+        # Iniciamos un ciclo
 		while error > tol:
-
 			i = i + 1		#estoy imprimindo la iteraccion
 
-			if pol(a)*pol(xr)<0:
+            # Verificamos con if si el resultado de las dos función
+			if pol(a)*pol(xr) < 0:
 				a=xr
 			else:
 				a=xra
-
+    
+            # Calculamos el resultado de la función, en esta linea es que va a cambiar
 			xr = a-(pol(a)/diff(a)) 	#aqui se
-
+   
+            # calculamos el error
 			error = abs((xr-xra)/xr)*100
-
+   
+            # Solo se imprimen los resultados
 			print ("{:^10}{:^10.6f}{:^10.6f}{:^10.6f}{:^10.6f}{:^10.6f}{:^10.6f}".format
-		  (i,float(a),float(pol(a)),float(diff(a)),float(xr),float(pol(xr)),float(error)))
+		        (
+                    i,
+                    float(a),
+                    float(pol(a)),
+                    float(diff(a)),
+                    float(xr),
+                    float(pol(xr)),
+                    float(error)
+                )
+            )
 
 			xra = xr
-
 	else:
-
 		print("No hay raices en el intervalo", "{",a,"}")
 
 # Llamada a la función principal
