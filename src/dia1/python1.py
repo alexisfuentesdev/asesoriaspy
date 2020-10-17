00323fe94f84c3ad84609d6bd10ebbada273a136
Alexis Fuentes <alexis.a.fuentes@gmail.com>
2020-10-16 19:44:02 -0500

    2

--- src/dia1/python1.py
+++ src/dia1/python1.py
@@ -11,3 +11,21 @@ if f > 30:
 else:
     print("No es")
     
+    
+# Tipos de datos
+Numero = 80
+Texto = "Esto es un texto"
+Flotante = 30.8
+Diccionario = {
+    "uno": 1,
+    "dos": "dos"
+}
+Tuplas = ["datos1", 343, 34.6, "sdlkjfsdjf", 398455, 345.5]
+
+# for dato in Tuplas:
+#     print(dato)
+
+# print(Tuplas[2])
+
+print(Diccionario["dos"])
+    
