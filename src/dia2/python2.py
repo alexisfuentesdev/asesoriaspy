74b15e0f6902a1aa631f67639aa961bc7eb6ba60
Alexis Fuentes <alexis.a.fuentes@gmail.com>
2020-10-18 20:23:46 -0500

    Casteo

--- src/dia2/python2.py
+++ src/dia2/python2.py
@@ -29,3 +29,34 @@ lambda
 print 
 while
 '''
+
+# print("*" * 60)
+
+f = "89h" #Asignación de valor
+# int  Podemos convertir cadenas de texto a enteros
+# float Para convertir cadenas de texto a flotantes o decimales
+# str Para convertir enteros o flotantes a cadenas de texto
+
+# int("4545") -> Entero
+
+# float("34.6") -> Flotante o decimal
+
+# str(3243) -> Cadena de texto
+
+
+# Bloque IF en Python
+if f.isnumeric(): # Validación de si la cadena es un número
+    d = int(f) # Cast a número
+    print(d * 10) # multiplicar 10
+else: # Si no se cumple la validación
+    print("\'f\' no es un número")
+
+print("*"*80)    
+
+
+entero = 60
+print(type(entero)) # imprimir el tipo de la variable
+print("-"*50)
+res = str(entero) # Cast a string o cadena
+print(res*10)
+print(type(res))
